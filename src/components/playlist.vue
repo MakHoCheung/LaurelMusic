7aa5d4d46b0ed61893d7df3b7d5cd223f9bbbc08
MakHoCheung <nathan_mak@outlook.com>
2018-10-10 22:21:22 +0800

    feat: 歌单页面歌单列表获取和显示实现

--- src/components/playlist.vue
+++ src/components/playlist.vue
@@ -13,7 +13,10 @@
             <el-tag v-for="(tag, index) in tags" :key="index">{{tag}}</el-tag>
         </el-row>
         <el-row>
-            <el-col :span="6">dd</el-col>
+            <el-col :span="6" v-for="(playList, index) in playLists" :key="index">
+                <img :src="playList.imgUrl" class="playlist-avatar"/>
+                <p>{{playList.name}}</p>
+            </el-col>
         </el-row>
         <el-row>
             <el-pagination layout="prev, pager, next" :total="50">
@@ -27,7 +30,8 @@ export default {
   name: "playlist",
   data: function() {
     return {
-      tags: []
+      tags: [],
+      playLists: []
     };
   },
   methods: {
@@ -38,12 +42,30 @@ export default {
           this.tags.push(element.name);
         });
       });
+    },
+    getPlayListWithTag(tag) {
+      let url = tag
+        ? "http://localhost:3000/top/playlist?limit=10&order=new&cat=" + tag
+        : "http://localhost:3000/top/playlist?limit=10&order=new";
+      axios.get(url).then(response => {
+        let dataList = response.data.playlists;
+        dataList.forEach(element => {
+          this.playLists.push({
+            imgUrl: element.coverImgUrl,
+            name: element.name
+          });
+        });
+      });
     }
   },
   mounted() {
-      this.getCatList();
+    this.getCatList();
+    this.getPlayListWithTag();
   }
 };
 </script>
 <style>
+.playlist-avatar{
+    width: 90%
+}
 </style>
